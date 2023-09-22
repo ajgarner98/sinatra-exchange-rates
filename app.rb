ae7ea7657f0f22f501312c5c4195aaed74efb0f8
Andre Garner <111984130+ajgarner98@users.noreply.github.com>
2023-09-22 03:54:17 +0000

    json parse not working

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
+
+
+
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @x = HTTP.get("https://api.exchangerate.host/symbols")
+  @y = JSON.parse(@x)
+  @z = @y.fetch("symbols")
+  @o = @z.fetch("AED")
+  erb(:home)
 end
