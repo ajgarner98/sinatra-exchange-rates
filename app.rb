773344717c59f5c1f46b0b2e0f8b84c43fdab84e
Andre Garner <111984130+ajgarner98@users.noreply.github.com>
2023-09-23 01:27:02 +0000

    Links to the convert currency page

--- app.rb
+++ app.rb
@@ -13,3 +13,13 @@ get("/") do
   @o = @z.fetch("AED")
   erb(:home)
 end
+
+get("/:currency") do
+  @q = params.fetch("currency")
+  @x = HTTP.get("https://api.exchangerate.host/symbols")
+  @y = JSON.parse(@x)
+  @z = @y.fetch("symbols")
+  @o = @z.fetch("AED")
+  @w = "cool"
+  erb(:currency)
+end
